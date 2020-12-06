fad4c5b79674160193be83fa5bb728503b2bdee2
sirKillface <pdalton1@umbc.edu>
2020-12-05 23:13:25 -0500

    added a morph suit model so as to be able to randomly generate their positions on the board

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+import random
+
 
 # Create your models here.
+class MorphSuit(models.Model):
+    suit_name = models.CharField(max_length=20, blank=True)
+    suit_x = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)])
+    suit_y = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)])
+    is_found= models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0}'.format(self.suit_name)
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,47 @@
 from django.shortcuts import render
+from .models import MorphSuit
+import random
 
 # Create your views here.
 def dashboard(request):
-    """dashboard renders a view for the homepage.
+    suitList = ["Red Suit", "Blue Suit", "Black Suit", "Pink Suit", "Yellow Suit", "White Suit", "Green Suit"]
+    for i in range(7):
+        x = random.randint(1, 8)
+        y = random.randint(1, 8)
+        morphSuit = MorphSuit(suit_name=suitList[i], suit_x=x, suit_y=y)
+        morphSuit.save()
 
-    Args:
-        request (HttpRequest): web request
+    context = {}
+    return render(request, 'dashboard/index.html', context)
 
-    Returns:
-        HttpResponse: text/html response
-    """
+def gameLoop(request):
+    p1 = random.randint(1, 6)
+    p2 = random.randint(1, 6)
+    p3 = random.randint(1, 6)
+    p4 = random.randint(1, 6)
+    p5 = random.randint(1, 6)
+    p6 = random.randint(1, 6)
+    p7 = random.randint(1, 6)
+    avg = round((p1+p2+p3+p4+p5+p6+p7)/7)
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(p5)
+    print(p6)
+    print(p7)
+    print("-------")
+    print(avg)
+    context = {"p1": p1,
+               "p2": p2,
+               "p3": p3,
+               "p4": p4,
+               "p5": p5,
+               "p6": p6,
+               "p7": p7,
+               "avg": avg}
+    return render(request, 'dashboard/gameBoard.html', context)
 
 
-    return render(request, 'dashboard/index.html', context={
 
-    })
+
